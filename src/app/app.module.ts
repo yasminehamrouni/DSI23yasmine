d32b9e61a00f926de91114b668b2349b02e73d1f
yasminehamrouni <148782794+yasminehamrouni@users.noreply.github.com>
2024-11-18 15:42:27 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule // Ajout du module de routage
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
