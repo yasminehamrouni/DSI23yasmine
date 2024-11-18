cddfdf5a016800e321ae3676287a7a313b090ed8
yasminehamrouni <148782794+yasminehamrouni@users.noreply.github.com>
2024-11-18 15:44:41 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,19 @@
 import { Routes } from "@angular/router";
 
 const routes:Routes({
+ { path: 'connexion', component: ConnexionComponent}
+  { path: 'acceuil', component: AcceuilComponent}
+ { path: 'inscription', component: InscriptionComponent}
+];
 
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
 
 })
